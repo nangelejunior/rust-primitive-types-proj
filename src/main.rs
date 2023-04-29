ff22a12a6b72520a913166873a3aab3eac3141e9
Neuclair.Junior <neuclair.junior@5by5.com.br>
2023-04-29 09:59:05 -0300

    out of bound error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
 	let mut numbers = [1.1, 2.0, 3.3];
-	println!("{:?}", numbers);
-	
-	numbers = [4.4, 5.5, 6.6]; 
-	println!("{:?}", numbers);
+	println!("{:?}", numbers[10]); //error
 }
