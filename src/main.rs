82a8d8cecd7aa60facf4c86514e5dab5729a3cb6
Neuclair.Junior <neuclair.junior@5by5.com.br>
2023-04-29 09:55:40 -0300

    pattern match with tuple

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
 	//tupla
-	let numbers = (1, 2, 3);	
-	println!("{:?}", numbers.1);
+	let numbers = (1, 2, 3);
+	println!("{:?}", numbers);
+	
+	let (a, b, c) = numbers;
+	println!("{:?}", a);
 }
