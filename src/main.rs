80f9ca9b21a447a0a7fac8af86b1cc63bed2932f
Neuclair.Junior <neuclair.junior@5by5.com.br>
2023-04-29 09:54:30 -0300

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 fn main() {
 	//tupla
-	let numbers = (1, 2, 3);
-	println!("{:?}", numbers);
-	
-	let numbers: (i32, i32, i32) = (1, 2, 3);
-	println!("{:?}", numbers);
-	
-	let numbers: (i32, i32, f64) = (1, 2, 3.5);
-	println!("{:?}", numbers);
+	let numbers = (1, 2, 3);	
+	println!("{:?}", numbers.1);
 }
