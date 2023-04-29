70657758e9d7d4c14d2fd8229cc68eec05861ee5
Neuclair.Junior <neuclair.junior@5by5.com.br>
2023-04-29 09:48:04 -0300

    float numbers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-	let x = 5;
-	let y = 199_456_898_9;
-	let h = 0xff;
-	let o = 0o77;
-	let b = 0b1111_0000;
-	let by = b'A';
+	let x = 42.1;
+	let x: f64 = 42.1; // f64 padrão -> precisão dupla
+	let x: f32 = 42.1;
 }
